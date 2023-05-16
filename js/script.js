efc8f41ac6308ebbe9997180b8db5117977979c4
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-05-16 09:36:36 +0200

    added to fixed

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ let message;
 button.addEventListener ("click", function() {
 
     let price = userTripDistance.value * 0.233;
-    
+
     if ( Age.value > 125  || Age.value < 0 || isNaN(Age.value)){
            message = 'You have entered an invalid age!'
              price = 0;
          } else if ( Age.value < 18 ){
             message = 'Since your traveler has less than 18 years old, you have a 19.4% discount on the final price.'
-            price = price - (price * (19.4 / 100));
+            price.toFixed = price - (price * (19.4 / 100));
          } else if ( Age.value >= 65){
            message = 'Since your traveler has more than 65 years old, you have a 37.7% discount on the final price.'
-            price = price - (price * (37.7 / 100));
+            price.toFixed = price - (price * (37.7 / 100));
          } else {
             message = 'Your traveler has received no discounts on the final price.'
          }
