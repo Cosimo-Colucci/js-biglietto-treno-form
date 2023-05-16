c5c52cbc04b038201d39f4cce2bd71507ca5833d
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-05-16 09:44:26 +0200

    MILESTONE 1

--- js/script.js
+++ js/script.js
@@ -12,27 +12,13 @@ button.addEventListener ("click", function() {
              price = 0;
          } else if ( Age.value < 18 ){
             message = 'Since your traveler has less than 18 years old, you have a 19.4% discount on the final price.'
-            price.toFixed = price - (price * (19.4 / 100));
+            price = price - (price * (19.4 / 100));
          } else if ( Age.value >= 65){
            message = 'Since your traveler has more than 65 years old, you have a 37.7% discount on the final price.'
-            price.toFixed = price - (price * (37.7 / 100));
+            price = price - (price * (37.7 / 100));
          } else {
             message = 'Your traveler has received no discounts on the final price.'
          }
          //document.getElementById('output').innerHTML += price.toFixed(2) +'€' + ' ' + message;
-    console.log (price)
+    console.log (price.toFixed(2))
 })
-
-// if ( inputeAge > 125  || inputeAge < 0 || isNaN(inputeAge)){
-//    message = 'You have entered an invalid age!'
-    // price = 0;
-// } else if ( inputeAge < 18 ){
-//    message = 'Since your traveler has less than 18 years old, you have a 19.4% discount on the final price.'
-//    price = price - (price * (19.4 / 100));
-// } else if ( inputeAge >= 65){
-//    message = 'Since your traveler has more than 65 years old, you have a 37.7% discount on the final price.'
-//    price = price - (price * (37.7 / 100));
-// } else {
-//    message = 'Your traveler has received no discounts on the final price.'
-// }
-// document.getElementById('output').innerHTML += price.toFixed(2) +'€' + ' ' + message;
